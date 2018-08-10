530f375989930bbd1f02e70a4e152e0a6904be02
Roger Melo <rogerw592@gmail.com>
2018-08-10 14:51:24 -0300

    add some 'this' exercises

--- tests/main.js
+++ tests/main.js
@@ -1,22 +1,10 @@
-function getOwnerName() {
-  return this.ownerName;
+function talk() {
+  console.log(this);
+  console.log(this.sound);
 }
 
-const car = {
-  model: 'A9',
-  brand: 'Audi',
-  ownerName: 'Roger Melo',
-  getOwnerName
+const animal = {
+  talk
 };
 
-const car2 = {
-  model: 'Silverado',
-  brand: 'Chevrolet',
-  ownerName: 'Vinícius',
-  getOwnerName
-};
-
-const audiOwnerName = car.getOwnerName.bind(car);
-const silveradoOwnwerName = car2.getOwnerName.bind(car2);
-
-console.log(silveradoOwnwerName());
+animal.talk();
